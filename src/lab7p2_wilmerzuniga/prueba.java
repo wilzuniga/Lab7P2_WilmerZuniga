226772f67618360933fefb1e87a12b6a03798f59
WilZuniga <wilmerzuniga.ant@gmail.com>
2022-09-02 13:50:53 -0600

    Pruebas con split

--- src/lab7p2_wilmerzuniga/prueba.java
+++ src/lab7p2_wilmerzuniga/prueba.java
@@ -10,5 +10,26 @@ package lab7p2_wilmerzuniga;
  * @author wilme
  */
 public class prueba {
-    
+
+    public static void main(String[] args) {
+        //String coso = "Ataque=100.0,Vida=400.0,Nombre=Clasico_Clasico:(Experiencia=3;Bandera=[Direccion:DiscoC,Color:Rojo])";
+        String coso = "Disparo:(Proyectil=Lechugas;Color=Verde)_Nombre=Lechuga,Vida=400.0,Rango=Medio,Ataque=300.0";
+
+        //coso.split
+        String[] cosos = coso.split("_");
+
+        for (int i = 0; i < cosos.length; i++) {
+            System.out.println(cosos[i]);
+//            if(cosos[i].equals(cosos))
+            String[] cososos = cosos[i].split(",");
+            for (int j = 0; j < cososos.length; j++) {
+                System.out.println(cososos[j]);
+            }
+
+
+        }
+        
+        
+    }
+
 }
